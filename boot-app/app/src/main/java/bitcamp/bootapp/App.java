0f67f7a07f29fffc3d19561f90d5d50bb1f5ea38
eomjinyoung <jinyoung.eom@gmail.com>
2022-12-26 17:36:20 +0900

    .

---
+++ boot-app/app/src/main/java/bitcamp/bootapp/App.java
@@ -0,0 +1,21 @@
+package bitcamp.bootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello, world!";
+  }
+}
