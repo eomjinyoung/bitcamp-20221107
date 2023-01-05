b32388570dcd7ad8e20f1e496f3ad367cb8f2786
eomjinyoung <jinyoung.eom@gmail.com>
2023-01-05 15:24:05 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -3,11 +3,21 @@ package bitcamp.myapp;
 public class App {
 
   public static void main(String[] args) {
-    MemberHandler.inputMembers();
+    while (true) {
+      System.out.println("1. 회원관리");
+      System.out.println("9. 종료");
+      int menuNo = Prompt.inputInt("메뉴> ");
 
-    System.out.println();
+      if (menuNo == 1) {
+        System.out.println("회원 관리입니다.");
+      } else if (menuNo == 9) {
+        break;
+      } else {
+        System.out.println("잘못된 메뉴 번호 입니다.");
+      }
+    }
 
-    MemberHandler.printMembers();
+    System.out.println("안녕히 가세요!");
   } // main()
 
 } // class App
