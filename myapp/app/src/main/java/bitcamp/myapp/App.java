125fcc6e842eb33a5486acf0e10a59abc3664ce2
eomjinyoung <jinyoung.eom@gmail.com>
2023-01-06 16:51:32 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -11,13 +11,31 @@ public class App {
   } // main()
 
   private static void goMainMenu() {
+    // 일반 학생 목록을 저장할 메모리를 준비한다.
+    MemberHandler generalMemberHandler = new MemberHandler("일반학생");
+
+    // 국비지원 학생 목록을 저장할 메모리를 준비한다.
+    MemberHandler supportedMemberHandler = new MemberHandler("국비지원학생");
+
+    // 기업 위탁 교육생 목록을 저장할 메모리를 준비한다.
+    MemberHandler companyMemberHandler = new MemberHandler("위탁교육생");
+
     while (true) {
-      System.out.println("1. 회원관리");
+      System.out.println("1. 일반학생관리");
+      System.out.println("2. 국비지원학생관리");
+      System.out.println("3. 위탁교육생관리");
       System.out.println("9. 종료");
       int menuNo = Prompt.inputInt("메뉴> ");
 
       if (menuNo == 1) {
-        MemberHandler.service();
+        generalMemberHandler.service();
+
+      } else if (menuNo == 2) {
+        supportedMemberHandler.service();
+
+      } else if (menuNo == 3) {
+        companyMemberHandler.service();
+
       } else if (menuNo == 9) {
         break;
       } else {
--- myapp/app/src/main/java/bitcamp/myapp/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/MemberHandler.java
@@ -4,13 +4,20 @@ import java.sql.Date;
 
 public class MemberHandler {
 
+  // 모든 인스턴스가 공유하는 데이터를 스태틱 필드로 만든다.
+  // 특히 데이터를 조회하는 용으로 사용하는 final 변수는 스태틱 필드로 만들어야 한다.
   static final int SIZE = 100;
-  static int count = 0;
 
-  // 레퍼런스 배열 준비
-  static Member[] members = new Member[SIZE];
+  int count;
+  Member[] members = new Member[SIZE];
+  String title;
 
-  static void inputMember() {
+  // 인스턴스를 만들 때 프롬프트 제목을 반드시 입력하도록 강제한다.
+  MemberHandler(String title) {
+    this.title = title;
+  }
+
+  void inputMember() {
     Member m = new Member();
     m.no = Prompt.inputInt("번호? ");
     m.name = Prompt.inputString("이름? ");
@@ -23,14 +30,14 @@ public class MemberHandler {
     m.level = (byte) Prompt.inputInt("0. 비전공자\n1. 준전공자\n2. 전공자\n전공? ");
     m.createdDate = new Date(System.currentTimeMillis()).toString();
 
-    members[count++] = m;
+    this.members[count++] = m;
   }
 
-  static void printMembers() {
+  void printMembers() {
     System.out.println("번호\t이름\t전화\t재직\t전공");
 
-    for (int i = 0; i < count; i++) {
-      Member m = members[i];
+    for (int i = 0; i < this.count; i++) {
+      Member m = this.members[i];
       System.out.printf("%d\t%s\t%s\t%s\t%s\n",
           m.no, m.name, m.tel,
           m.working ? "예" : "아니오",
@@ -38,10 +45,10 @@ public class MemberHandler {
     }
   }
 
-  static void printMember() {
+  void printMember() {
     int memberNo = Prompt.inputInt("회원번호? ");
 
-    Member m = findByNo(memberNo);
+    Member m = this.findByNo(memberNo);
 
     if (m == null) {
       System.out.println("해당 번호의 회원이 없습니다.");
@@ -59,6 +66,8 @@ public class MemberHandler {
     System.out.printf("  등록일: %s\n", m.createdDate);
   }
 
+  // 인스턴스 멤버(필드나 메서드)를 사용하지 않기 때문에
+  // 그냥 스태틱 메서드로 두어라!
   static String getLevelText(int level) {
     switch (level) {
       case 0: return "비전공자";
@@ -67,10 +76,10 @@ public class MemberHandler {
     }
   }
 
-  static void modifyMember() {
+  void modifyMember() {
     int memberNo = Prompt.inputInt("회원번호? ");
 
-    Member old = findByNo(memberNo);
+    Member old = this.findByNo(memberNo);
 
     if (old == null) {
       System.out.println("해당 번호의 회원이 없습니다.");
@@ -98,7 +107,7 @@ public class MemberHandler {
 
     String str = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
     if (str.equalsIgnoreCase("Y")) {
-      members[indexOf(old)] = m;
+      this.members[this.indexOf(old)] = m;
       System.out.println("변경했습니다.");
     } else {
       System.out.println("변경 취소했습니다.");
@@ -106,10 +115,10 @@ public class MemberHandler {
 
   }
 
-  static void deleteMember() {
+  void deleteMember() {
     int memberNo = Prompt.inputInt("회원번호? ");
 
-    Member m = findByNo(memberNo);
+    Member m = this.findByNo(memberNo);
 
     if (m == null) {
       System.out.println("해당 번호의 회원이 없습니다.");
@@ -122,51 +131,69 @@ public class MemberHandler {
       return;
     }
 
-    for (int i = indexOf(m) + 1; i < count; i++) {
-      members[i - 1] = members[i];
+    for (int i = this.indexOf(m) + 1; i < this.count; i++) {
+      this.members[i - 1] = this.members[i];
     }
-    members[--count] = null; // 레퍼런스 카운트를 줄인다.
+    this.members[--this.count] = null; // 레퍼런스 카운트를 줄인다.
 
     System.out.println("삭제했습니다.");
 
   }
 
-  static Member findByNo(int no) {
-    for (int i = 0; i < count; i++) {
-      if (members[i].no == no) {
-        return members[i];
+  Member findByNo(int no) {
+    for (int i = 0; i < this.count; i++) {
+      if (this.members[i].no == no) {
+        return this.members[i];
       }
     }
     return null;
   }
 
-  static int indexOf(Member m) {
-    for (int i = 0; i < count; i++) {
-      if (members[i].no == m.no) {
+  int indexOf(Member m) {
+    for (int i = 0; i < this.count; i++) {
+      if (this.members[i].no == m.no) {
         return i;
       }
     }
     return -1;
   }
 
-  static void service() {
+  void searchMember() {
+    String name = Prompt.inputString("이름? ");
+
+    System.out.println("번호\t이름\t전화\t재직\t전공");
+
+    for (int i = 0; i < this.count; i++) {
+      Member m = this.members[i];
+      if (m.name.equalsIgnoreCase(name)) {
+        System.out.printf("%d\t%s\t%s\t%s\t%s\n",
+            m.no, m.name, m.tel,
+            m.working ? "예" : "아니오",
+                getLevelText(m.level));
+      }
+    }
+  }
+
+  void service() {
     while (true) {
-      System.out.println("[회원 관리]");
+      System.out.printf("[%s]\n", this.title);
       System.out.println("1. 등록");
       System.out.println("2. 목록");
       System.out.println("3. 조회");
       System.out.println("4. 변경");
       System.out.println("5. 삭제");
+      System.out.println("6. 검색");
       System.out.println("0. 이전");
-      int menuNo = Prompt.inputInt("회원관리> ");
+      int menuNo = Prompt.inputInt(String.format("%s> ", this.title));
 
       switch (menuNo) {
         case 0: return;
-        case 1: inputMember(); break;
-        case 2: printMembers(); break;
-        case 3: printMember(); break;
-        case 4: modifyMember(); break;
-        case 5: deleteMember(); break;
+        case 1: this.inputMember(); break;
+        case 2: this.printMembers(); break;
+        case 3: this.printMember(); break;
+        case 4: this.modifyMember(); break;
+        case 5: this.deleteMember(); break;
+        case 6: this.searchMember(); break;
         default:
           System.out.println("잘못된 메뉴 번호 입니다.");
       }
