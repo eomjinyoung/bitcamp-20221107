ff12a4287f8cd5270dbefd6eec51133bad7a5dcf
eomjinyoung <jinyoung.eom@gmail.com>
2023-01-10 11:27:16 +0900

    .

--- backend-app/app/src/main/java/bitcamp/bootapp/controller/BoardController.java
+++ backend-app/app/src/main/java/bitcamp/bootapp/controller/BoardController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import bitcamp.bootapp.dao.BoardDao;
 import bitcamp.bootapp.vo.Board;
@@ -23,6 +24,25 @@ public class BoardController {
     b.setViewCount(1);
 
     this.boardDao.insert(b);
+  }
+
+  @PostMapping("/boards")
+  public Object getBoard(@PathVariable int boardNo) {
+
+    Board b = this.boardDao.findByNo(boardNo);
+
+    // 응답 결과를 담을 맵 객체 준비
+    Map<String,Object> contentMap = new HashMap<>();
+
+    if (b == null) {
+      contentMap.put("status", "failure");
+      contentMap.put("message", "해당 번호의 게시글이 없습니다.");
+    } else {
+      contentMap.put("status", "success");
+      contentMap.put("message", b);
+    }
+
+    return contentMap;
   }
 
   @GetMapping("/boards/{boardNo}")
