31b7f910760fbb63cff08b8864ee9ffb453e69a9
eomjinyoung <jinyoung.eom@gmail.com>
2023-01-27 17:20:08 +0900

    .

---
+++ myapp/app-24/src/main/java/bitcamp/myapp/dao/BoardDao.java
@@ -0,0 +1,77 @@
+package bitcamp.myapp.dao;
+
+import java.sql.Date;
+import bitcamp.myapp.vo.Board;
+import bitcamp.util.Iterator;
+import bitcamp.util.List;
+
+public class BoardDao {
+
+  List<Board> list;
+
+  public BoardDao(List<Board> list) {
+    this.list = list;
+  }
+
+  int lastNo;
+
+  public void insert(Board board) {
+    board.setNo(++lastNo);
+    board.setCreatedDate(new Date(System.currentTimeMillis()).toString());
+    list.add(board);
+  }
+
+  public Board[] findAll() {
+    Board[] boards = new Board[list.size()];
+    Iterator<Board> i = list.iterator();
+    int index = 0;
+    while (i.hasNext()) {
+      boards[index++] = i.next();
+    }
+    return boards;
+  }
+
+  public Board findByNo(int no) {
+    Board b = new Board();
+    b.setNo(no);
+
+    int index = list.indexOf(b);
+    if (index == -1) {
+      return null;
+    }
+
+    return list.get(index);
+  }
+
+  public void update(Board b) {
+    int index = list.indexOf(b);
+    list.set(index, b);
+  }
+
+  public boolean delete(Board b) {
+    return list.remove(b);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
