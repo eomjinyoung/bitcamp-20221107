3c6f14eb8790e10ee425d668776837b322648091
eomjinyoung <jinyoung.eom@gmail.com>
2023-02-13 10:11:37 +0900

    .

---
+++ myapp/app-37-client/src/main/java/bitcamp/myapp/ClientApp.java
@@ -0,0 +1,51 @@
+package bitcamp.myapp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientApp {
+
+  public static void main(String[] args) {
+    new ClientApp().execute("localhost", 8888);
+  }
+
+  void execute(String ip, int port) {
+    try (Scanner keyboard = new Scanner(System.in);
+        Socket socket = new Socket(ip, port);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+
+      System.out.print(in.readUTF());
+
+      while (true) {
+        String input = prompt("> ", keyboard);
+        out.writeUTF(input);
+
+        String response = in.readUTF();
+        if (response.equalsIgnoreCase("quit")) {
+          break;
+        }
+        System.out.print(response);
+      }
+      System.out.println("안녕!");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private String prompt(String title, Scanner keyboard) throws Exception {
+    System.out.printf("%s", title);
+    return keyboard.nextLine();
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ myapp/app-37-server/src/main/java/bitcamp/myapp/dao/StudentDao.java
@@ -0,0 +1,26 @@
+package bitcamp.myapp.dao;
+
+import bitcamp.myapp.vo.Student;
+
+public interface StudentDao {
+
+  void insert(Student s);
+
+  Student[] findAll();
+
+  Student findByNo(int no);
+
+  Student[] findByKeyword(String keyword);
+
+  void update(Student s);
+
+  boolean delete(Student s);
+
+}
+
+
+
+
+
+
+
