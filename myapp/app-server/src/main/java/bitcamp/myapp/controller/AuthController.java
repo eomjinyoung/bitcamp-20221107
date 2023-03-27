744afba68e364bc33540a98b1fcafa1df01d5c42
eomjinyoung <jinyoung.eom@gmail.com>
2023-03-27 15:14:41 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -1,12 +1,15 @@
 package bitcamp.myapp.controller;
 
+import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import bitcamp.myapp.service.StudentService;
 import bitcamp.myapp.service.TeacherService;
 import bitcamp.myapp.vo.Member;
@@ -72,6 +75,27 @@ public class AuthController {
       return new RestResult()
           .setStatus(RestStatus.FAILURE);
     }
+  }
+
+  @PostMapping("facebookLogin")
+  public Object facebookLogin(
+      @RequestBody Map<String,String> jsonData,
+      HttpSession session) {
+
+    // 클라이언트가 보낸 accessToken을 가지고
+    // 페이스북 서버에 접속(AJAX 요청)하여 사용자를 정보를 가져온다.
+    //
+    RestTemplate restTemplate = new RestTemplate();
+    String result = restTemplate.getForObject(
+        "https://graph.facebook.com/v16.0/me?access_token={value1}&fields={value2}", // 요청할 URL
+        String.class, // 서버에서 받은 결과의 타입
+        jsonData.get("accessToken"), // URL의 첫 번째 자리에 들어갈 값
+        "id,name,email,gender" // 페이스북 측에 요청하는 로그인 사용자 정보
+        );
+
+    return new RestResult()
+        .setStatus(RestStatus.SUCCESS)
+        .setData(result);
   }
 
 }
